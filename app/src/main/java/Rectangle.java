f24c75b8fe9939164264114f7fa3234115af5f19
MileyCao <cao4@ualberta.ca>
2019-01-30 18:41:52 -0700

    rectangle build

---
+++ app/src/main/java/Rectangle.java
@@ -0,0 +1,14 @@
+public abstract class Rectangle extends Shape {
+    protected int x;
+    protected int y;
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public abstract void setX(int x);
+    public abstract void setY(int Y);
+}
