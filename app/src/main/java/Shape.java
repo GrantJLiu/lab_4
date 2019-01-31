1adc4d3d61e05d94280d0564e4bd69e1ef95400e
uKira <darkra98@gmail.com>
2019-01-30 18:23:42 -0700

    Added abstract Shape

---
+++ app/src/main/java/Shape.java
@@ -0,0 +1,15 @@
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public abstract int setX(int x);
+    public abstract int setY(int Y);
+}
